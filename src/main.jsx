47f438f20de0dbe156f8d127bad8cfd76da303fd
unknown <vermaaadityaff123@gmail.com>
2026-09-05 15:42:04 +0530

    Switch back to clean BrowserRouter for Vercel deployment

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { AppProvider } from './context/AppContext';
 import { App } from './App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <AppProvider>
         <App />
       </AppProvider>
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
+
 
