e24913a0bb2d66f9f6bcb2dff6fa86d74f0d1373
unknown <vermaaadityaff123@gmail.com>
2026-09-05 15:51:57 +0530

    Set router to HashRouter exclusively as requested

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { AppProvider } from './context/AppContext';
 import { App } from './App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <AppProvider>
         <App />
       </AppProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
+
 
 
